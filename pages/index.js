d08847b39bb20ab2ed7c0a626292048df783da8a
OnJACK <onjacksm@gmail.com>
2022-11-14 18:56:08 -0300

    feat: light/dark mode theme

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,14 @@
 import React from "react";
 import config from "../config.json";
 import styled from "styled-components";
-import { CSSReset } from "../src/components/CSSReset";
 import Menu from "../src/components/Menu";
 import { StyledTimeline } from "../src/components/TimeLineStyle";
 
 function HomePage() {
-    const estiloDaHome = {
-        //backgroundColor: "red" 
-    };
-
-    // console.log(config.playlists);
-
     const [valorDoFiltro, setValorDoFiltro] = React.useState("");
 
     return (
         <>
-            <CSSReset />
             <div style={{
                 display: "flex",
                 flexDirection: "column",
@@ -49,6 +41,8 @@ export default HomePage
 // };
 
 const StyledHeader = styled.div`
+    background-color: ${({ theme }) => theme.backgroundLevel1};
+
     .user-img {
         width: 80px;
         height: 80px;
